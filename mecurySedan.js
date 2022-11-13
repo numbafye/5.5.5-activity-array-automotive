ed08988e25380c96a1079137a23965da06c27439
numbafye <saadiqgp@gmail.com>
2022-11-13 10:17:21 -0500

    added passenger from methods chart

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,33 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberofWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+
+loadPassenger (num) {
+    if (this.passenger < this.maxPassengers) {
+        if ((num + this.passenger) <= this.maxPassengers) {
+        this.passenger = num;
+        return this.passenger;
+    } else {
+        console.log(this.model + " " + "not have enough space to take all passengers.");
+    }
+} else {
+    console.log(this.model + " " + this.make + "is full");
+}
+
+}
+}
+
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
